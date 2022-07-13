168e0892c25f89b3af02c3aa8e53e4393e283391
Robert Lopez <rplopez720@gmail.com>
2022-07-13 14:24:59 -0400

    Some movement.

--- Ex_Files_React_js_EssT/React Design Patterns/Layout Components/People/SmallPersonListIItem.js
+++
@@ -1,9 +0,0 @@
-export const SmallPersonListItem = ({ person }) => {
-    const { name, age} = person ;
-
-    return (
-        <p>Names: {name}, Age: {age} years</p>
-    );
-}
-
-//There is no styling indicating how the item is gonna be displayed 
